5f3d5acc9bc19e3b3bec8328a5a4d183728611d8
JohnVDF <johnvicdf@hotmail.com>
2025-12-11 11:37:41 +0100

    L1

---
+++ contracts/LPO.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract LPO is ERC20, ERC20Burnable {
+    constructor() ERC20("LPO", "MTK") {}
+}
